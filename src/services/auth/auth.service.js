2e88ede032aa370220c240626684e1ed916d5c26
Fatimə <guliyeffa2l@gmail.com>
2025-10-15 05:44:48 +0300

    Add user authentication (login)

--- src/services/auth/auth.service.js
+++ src/services/auth/auth.service.js
@@ -3,14 +3,36 @@ import { $api } from "@/api/api";
 import { API } from "@/api/endpoints";
 
 export class AuthService {
-  constructor() {
-    this.userData = {};
-  }
+  static userData = {};
+
   static getUserData() {
     return this.userData;
   }
 
-  static login(payload) {
-    return $axios.post($api(API.login),payload).then(data=> data)
+  static isLoggedIn() {
+    return !!localStorage.getItem("accessToken");
+  }
+
+  static async login(payload) {
+    try {
+      const response = await $axios.post($api(API.login), payload);
+
+      if (response.data.success) {
+        alert("Uğurla daxil oldun!");
+        localStorage.setItem("accessToken", response.data.data.accessToken);
+        localStorage.setItem("refreshToken", response.data.data.refreshToken);
+
+        this.userData = response.data.data;
+      } 
+      else {
+        alert(response.data.errorMessage || "Login uğursuz oldu!");
+      }
+      return response.data;
+    } 
+    catch (error) {
+      console.error("Login error:", error);
+      alert("Serverlə əlaqə qurulmadı.");
+      throw error;
+    }
   }
 }
