dc13e5c4b48bf91bc1851b512902136e1fa4e755
Fatimə <guliyeffa2l@gmail.com>
2025-10-27 22:15:49 +0300

    add admin panel routes and sidebar

--- src/layouts/global.layout.jsx
+++ src/layouts/global.layout.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-import Lenis from "@studio-freight/lenis";
+import Lenis from 'lenis';
 import { Outlet } from "react-router-dom";
 
 function GlobalLayout() {
---
+++ src/pages/admin/photos/add-photo.page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function AddPhotoPage() {
+  return (
+ <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+            <div className="grid gap-4">
+                <div className="bg-muted/50 h-20 rounded-xl" />
+                <div className="bg-muted/50 h-20 rounded-xl" />
+                <div className="bg-muted/50 h-20 rounded-xl" />
+                <div className="bg-muted/50 h-20 rounded-xl" />
+                <div className="bg-muted/50 h-20 rounded-xl" />
+            </div>
+        </div>  )
+}
+
+export default AddPhotoPage
